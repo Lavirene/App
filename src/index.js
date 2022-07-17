66d1b701ea4b36f3ac9be0a1e2106219520565d9
Lavirene <108747106+Lavirene@users.noreply.github.com>
2022-07-17 20:12:39 +0300

    Style Improvements.

--- src/index.js
+++ src/index.js
@@ -27,47 +27,97 @@ let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
-function search(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
+let celciusTemperature = null;
+
+
+function search(city) {
   let apiKey = `82d623942976c17e87d20abb94fc530f`;
   let units = "metric";
-  let city = cityInput.value.toUpperCase();
-  if (cityInput.value.length < 1) city = "Kyiv";
-
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(url).then(displayWeather);
 }
 
+
+function submitSearch(event) {
+
+  event.preventDefault();
+
+  let cityInput = document.querySelector("#city-input");
+
+  let city = cityInput.value.toUpperCase();
+  if (cityInput.value.length < 1) city = "Kyiv";
+
+  search(city);
+}
+
 function displayWeather(response) {
-  let temperature = Math.round(response.data.main.temp);
+
+  celciusTemperature = Math.round(response.data.main.temp);
+  let imageUrl = response.data.weather[0].icon;
+  let imageLink = `http://openweathermap.org/img/wn/${imageUrl}@2x.png`;
 
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
-  let weatherDescription = document.querySelector("#weatherDescription");
+  let imageElement = document.querySelector("#weather-icon");
+  let weatherDescription = document.querySelector("#weather-description");
   let wind = document.querySelector("#wind");
   let humidity = document.querySelector("#humidity");
 
   cityElement.innerHTML = response.data.name;
-  temperatureElement.innerHTML = temperature;
+  temperatureElement.innerHTML = celciusTemperature;
+  imageElement.setAttribute('src', imageLink);
   weatherDescription.innerHTML = response.data.weather[0].main;
   wind.innerHTML = response.data.wind.speed;
   humidity.innerHTML = response.data.main.humidity;
+
+}
+
+function convertToFahrenheit(event) {
+    event.preventDefault();
+    if ( celciusTemperature != null ) {
+        celciusSwitcher.classList.remove('active')
+        fahrenheitSwitcher.classList.add('active')
+
+        let fahrenheitTemperature = Math.round(celciusTemperature * 5 / 9 - 32);
+        let temperatureElement = document.querySelector("#temperature");
+        temperatureElement.innerHTML = fahrenheitTemperature;
+    }
+}
+
+function convertToCelcius() {
+    event.preventDefault();
+
+    celciusSwitcher.classList.add('active')
+    fahrenheitSwitcher.classList.remove('active')
+
+    let temperatureElement = document.querySelector("#temperature");
+    console.log(celciusTemperature)
+    temperatureElement.innerHTML = celciusTemperature;
+
+
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", submitSearch);
 
-let currentTempButton = document.querySelector("#getLocationTemp");
-currentTempButton.addEventListener("click", function () {
-  navigator.geolocation.getCurrentPosition(retrievePosition);
-});
+let fahrenheitSwitcher = document.querySelector('#fahrenheitSwitcher');
+fahrenheitSwitcher.addEventListener('click', convertToFahrenheit);
 
-function retrievePosition(position) {
-  let apiKey = `82d623942976c17e87d20abb94fc530f`;
-  let units = "metric";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-  axios.get(url).then(displayWeather);
-}
+let celciusSwitcher = document.querySelector('#celciusSwitcher');
+celciusSwitcher.addEventListener('click', convertToCelcius);
+
+search("Kyiv");
+
+// let currentTempButton = document.querySelector("#getLocationTemp");
+// currentTempButton.addEventListener("click", function () {
+  // navigator.geolocation.getCurrentPosition(retrievePosition);
+// });
+
+// function retrievePosition(position) {
+//   let apiKey = `82d623942976c17e87d20abb94fc530f`;
+//   let units = "metric";
+//   let lat = position.coords.latitude;
+//   let lon = position.coords.longitude;
+//   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+//   axios.get(url).then(displayWeather);
+// }
